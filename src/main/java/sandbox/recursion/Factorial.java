fb8e7ce27560ba99bf247323ec28623a5e1bff69
antoniabgm <ant2249273@maricopa.edu>
2024-12-04 18:14:36 -0700

    adding recursion

---
+++ src/main/java/sandbox/recursion/Factorial.java
@@ -0,0 +1,25 @@
+package sandbox.recursion;
+
+public class Factorial {
+
+    private static int count = 0;
+
+    public static void main(String[] args) {
+
+        for (int x = 0; x <= 20; x++) {
+            System.out.println("Factorial of " + x + " is " + factorial(x));
+        }
+    }
+
+    public static long factorial(int x){
+
+        if(x == 0){
+            return 1;
+        }
+
+        count++;
+        System.out.println("Count: " + count);
+
+        return x * factorial(x - 1);
+    }
+}
--- src/main/java/sandbox/recursion/FibonacciNumbers.java
+++ src/main/java/sandbox/recursion/FibonacciNumbers.java
@@ -5,26 +5,35 @@ import java.util.Map;
 
 public class FibonacciNumbers {
 
-    private Map<Integer, Long> dictionary;
+    private Map<Integer, Long> dictionary = new HashMap<>();
 
-    public FibonacciNumbers() {
-        this.dictionary = new HashMap<>();
+    public static void main(String[] args) {
+
+        FibonacciNumbers fn = new FibonacciNumbers();
+
+        for (int n = 0; n <= 92; n++) {
+            System.out.println("fib(" + n + ") = " + (fn.fib(n)));
+        }
     }
 
-    public long fib(int n) {
+    // O(2^n) amortized to O(1) using memoization
+    public long fib(int x) {
 
-        if(n == 0) {
-            return 0;
+        // f0 = 0
+        if (x == 0) {
+            return 0L;
         }
 
-        if (n == 1) {
-            return 1;
+        // f1 = 1
+        if (x == 1) {
+            return 1L;
         }
 
-        return memo(n-1) + memo(n - 2);
+        // fn = fn-1 + fn-2
+        return memo(x - 1) + memo(x - 2);
     }
 
-    private long memo(int m) {
+    public long memo(int m) {
 
         Long value = dictionary.get(m);
 
